5097c510de6716c1f57f481e842f48a25145a836
koensmallegange <70605029+koensmallegange@users.noreply.github.com>
2021-01-06 16:46:30 +0100

    improved visualisation code

--- code/visualisatie.py
+++ code/visualisatie.py
@@ -1,3 +1,7 @@
+# By Koen Smallegange 
+# For programmeertheorie - SmartTeam25
+# Splits data from dataReader.py and plots houses and batteries for 1 district
+
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import (AutoMinorLocator, MultipleLocator)
@@ -13,36 +17,49 @@ house_y = []
 bat_x = []
 bat_y = []
 
+        
+# gets data from dataReader.py
+def get_data():
+    for house in houses:
+        house_x.append(int(house.x))
+        house_y.append(int(house.y))
 
-# get data
-for house in houses:
-    house_x.append(int(house.x))
-    house_y.append(int(house.y))
+    for battery in batteries:
+        bat_x.append(int(battery.x))
+        bat_y.append(int(battery.y))
+    
 
-for battery in batteries:
-    bat_x.append(int(battery.x))
-    bat_y.append(int(battery.y))
+# plots single grid with seperate x and y cordslist for houses and batteries
+def plot():
+    icon_size = 9
+
+    fig, ax = plt.subplots(1, figsize=(10, 10))
+    fig.suptitle('District 1')
+
+    ax.plot(house_x, house_y, 'p', color = 'blue', label = 'houses', markersize = icon_size)
+    ax.legend()
+    ax.plot(bat_x, bat_y, 's', color = 'red', label = 'batteries', markersize = icon_size)
+    ax.legend()
+
+    ax.xaxis.set_minor_locator(AutoMinorLocator(10))
+    ax.yaxis.set_minor_locator(AutoMinorLocator(10))
+
+    plt.xlim(-1, 51)
+    plt.ylim(-1, 51)
+
+    plt.grid(b=True, which='major', color='black', linestyle='-')
+    plt.grid(b=True, which='minor', color='black', linestyle='-', alpha = 0.2)
+
+    plt.show()
 
 
-# plot district
-fig, ax = plt.subplots(1, figsize=(10, 10))
-fig.suptitle('District 1')
+# controls flow of code, data and variables
+def main():
+    get_data()
+    plot()
 
-ax.plot(house_x, house_y, 'o', color = 'blue', label = 'houses', markersize = 7)
-ax.legend()
-ax.plot(bat_x, bat_y, 'o', color = 'red', label = 'batteries', markersize = 7)
-ax.legend()
-
-ax.xaxis.set_minor_locator(AutoMinorLocator(10))
-ax.yaxis.set_minor_locator(AutoMinorLocator(10))
-
-plt.xlim(-1, 51)
-plt.ylim(-1, 51)
-
-plt.grid(b=True, which='major', color='black', linestyle='-')
-plt.grid(b=True, which='minor', color='black', linestyle='-', alpha = 0.2)
-
-plt.show()
+# run code
+main()
 
 
 
