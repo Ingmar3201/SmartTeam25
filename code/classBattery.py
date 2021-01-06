8f217b5db9895ab0bff7287b153457f801666270
Ingmar3201 <ingmar.demare@gmail.com>
2021-01-06 11:07:36 +0100

    merge

--- code/classBattery.py
+++ code/classBattery.py
@@ -1,12 +1,17 @@
 class CBattery():
     def __init__(self, position, capacity):
         self._position = position
-        self._capacity = capacity
+        self.capacity = capacity
 
     def coordinates(self):
-        self._x = 0
-        self._y = 0
-        """ 
-        for letter in position:
-            if isnumeric(letter):
-                 """
+        self.x = 0
+        self.y = 0
+        temp = ""
+
+        for letter in self._position:
+            if letter == ",":
+                self.x = int(temp)
+                temp = ""
+            else:
+                temp += letter
+        self.y = int(temp)
--- code/dataReader.py
+++ code/dataReader.py
@@ -1,16 +1,39 @@
 import csv
+from classBattery import CBattery
+from classHouse import House
 
-with open("../data/district_1/district-1_batteries.csv", 'r') as file:
+"""
+house objects by Freek
+"""
+houses = []
+
+with open("../data/district_1/district-1_houses.csv", 'r') as file:
     reader = csv.reader(file)
     next(file, None)
     for row in reader:
-        position = row[0]
+        #print(row)
+        houses.append(House(row[0], row[1], row[2]))
+"""
+print("---")
+for i in houses:
+    i.test()
+"""
 
-x = 0
-y = 0
-temp = ""
+"""
+battery objects by Ingmar
+"""
+batteries = []
 
-for letter in position:
-    if letter != ",":
-        temp += letter
-        print(temp)
+with open("../data/district_1/district-1_batteries.csv", 'r') as file:
+    reader = csv.reader(file)
+    next(reader, None)
+    for row in reader:
+        battery = CBattery(row[0], row[1])
+        battery.coordinates()
+        batteries.append(battery)
+
+"""
+for bat in batteries:
+    print(bat.x, bat.y)
+    print(bat.capacity)
+    """
