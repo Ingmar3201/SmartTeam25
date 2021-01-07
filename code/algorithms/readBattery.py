b311062e5f44094fb99084d3a4b6fd5a537415ed
Ingmar3201 <ingmar.demare@gmail.com>
2021-01-07 11:18:38 +0100

    return battery

--- code/algorithms/readBattery.py
+++ code/algorithms/readBattery.py
@@ -6,9 +6,7 @@ import os, sys
 import csv
 
 directory = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(directory, "code"))
 sys.path.append(os.path.join(directory, "code", "classes"))
-sys.path.append(os.path.join(directory, "code", "algorithms"))
 
 from classBattery import Battery
 
@@ -22,6 +20,8 @@ def readBattery(batteryData):
             battery = Battery(row[0], row[1])
             battery.extract_coordinates()
             batteries.append(battery)
+        
+    return batteries
 
 
     #for bat in batteries:
