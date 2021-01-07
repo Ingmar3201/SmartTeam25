db22dfed58ba35974dff90d19c13f44c5c7b6b38
Ingmar3201 <ingmar.demare@gmail.com>
2021-01-07 10:33:08 +0100

    file structure update

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import csv
+import os, sys
+
+
+directory = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(directory, "code"))
+sys.path.append(os.path.join(directory, "code", "classes"))
+sys.path.append(os.path.join(directory, "code", "algoritmes"))
+
 from classBattery import Battery
 from classHouse import House
+
 
 """
 house objects by Freek
 """
 houses = []
 
-with open("../data/district_1/district-1_houses.csv", 'r') as file:
+with open("data/district_1/district-1_houses.csv", 'r') as file:
     reader = csv.reader(file)
     next(file, None)
     for row in reader:
@@ -24,7 +33,7 @@ battery objects by Ingmar
 """
 batteries = []
 
-with open("../data/district_1/district-1_batteries.csv", 'r') as file:
+with open("data/district_1/district-1_batteries.csv", 'r') as file:
     reader = csv.reader(file)
     next(reader, None)
     for row in reader:
