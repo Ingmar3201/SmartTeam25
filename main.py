a2e8ecedd9fa16d4e2a41e1da8fbbf35e84a6abf
koensmallegange <70605029+koensmallegange@users.noreply.github.com>
2021-01-07 11:48:26 +0100

    pathname as variable in main

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ directory = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(directory, "code"))
 sys.path.append(os.path.join(directory, "code", "classes"))
 sys.path.append(os.path.join(directory, "code", "algorithms"))
+sys.path.append(os.path.join(directory, "code", "visualisation"))
 
 from classBattery import Battery
 from classHouse import House
 from readBattery import readBattery
 from readHouse import readHouse
+from vis import plot
 
-readHouse("data/district_1/district-1_houses.csv")
+housePath = "data/district_1/district-1_houses.csv"
+batteryPath =  "data/district_1/district-1_batteries.csv"
 
-readBattery("data/district_1/district-1_batteries.csv")
+readHouse(housePath)
+readBattery(batteryPath)
+plot(housePath, batteryPath)
