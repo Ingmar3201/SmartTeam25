d2b87f583daffc8b4cc483f4d02c9752fa2fc1a0
Ingmar3201 <ingmar.demare@gmail.com>
2021-01-11 09:52:44 +0100

    calLength

---
+++ code/algorithms/objective.py
@@ -0,0 +1,25 @@
+
+from classCable import Cable
+
+def objective():
+    """
+    A class to calculate the objective function
+    The objective function is the total price of the network
+    """
+    def __init__(self, cables, batteries):
+        self.cables = cables
+        self.batteries = batteries
+        self._segmentCost = 9
+        self._batteryCost = 5000
+    
+    def totalCost(self):
+        cableSum = 0
+        for cable in self.cables:
+            cableSum += cable.calcLength()
+
+        batterySum = len(self.batteries)
+
+        total = cableSum * self._segmentCost + batterySum * self._batteryCost
+        
+        return total
+
--- code/classes/classCable.py
+++ code/classes/classCable.py
@@ -8,11 +8,11 @@ class Cable():
         self.x = [house.x, battery.x, battery.x]
         self.y = [house.y, house.y, battery.y]
 
-""" 
     def calcLength(self):
         length = abs(self.house.x - self.battery.x) + abs(self.house.y - self.battery.y)
         return length
 
+"""
     def createRoute(self):
         start = (self.house.x, self.house.y)
         corner = (self.battery.x , self.house.y)
