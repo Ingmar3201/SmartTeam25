7cb69f86c7a6a3a7f12df3d149e3d9ea192d00d9
koensmallegange <70605029+koensmallegange@users.noreply.github.com>
2021-01-27 18:08:55 +0100

    added comments to class share

--- code/algorithms/classShare.py
+++ code/algorithms/classShare.py
@@ -2,13 +2,14 @@ from classDensity import Density
 
 class Share(Density):
     '''
-    Enables cablesharing between houses
+    Enables cablesharing between houses by searching for houses lying on parallel cables in the x direction 
     '''
     
     def runShare(self):
         '''
         Control flow of share algorithm
         '''
+
         # get initial solution (density method from classdensity)
         self.runDensity()
         lowestCost = self.totalCost()
@@ -19,11 +20,11 @@ class Share(Density):
         self.initialCables = self.clone()
         bestSolution = self.clone()
 
-        # set limiting parameters: the max amount of times a worse cost is accepted
+        # set limiting parameters
         maxDeteriorations = 5
         deteriorations = 0
 
-        # every iteration scan one step further for houses
+        # scan one step further for houses as long as new configuration within limiting parameters
         while deteriorations < maxDeteriorations:
             self.verticalSteps += 1
             
@@ -41,7 +42,7 @@ class Share(Density):
                 self.relayCables()
                 rep += 1
             
-            # when a better configuration if found save it and reset the limiting parameters
+            # when a better configuration is found save it and reset the limiting parameters
             if self.totalCost() < lowestCost:
                 deteriorations = 0
                 lowestCost = self.totalCost()
@@ -60,15 +61,15 @@ class Share(Density):
     def relayCables(self):
         '''
         merge cables by making connections between parallel cables 
-
         '''
+
         # per battery sort houses from furthest to closest to battery
         for battery in self.batteries:
             houses = self.housesPerBattery(battery)
             blacklistHouses = []
             houses = self.sortHouseDistance(houses)
 
-            # per house scan for nearby cables belonging to the same battery
+            # per house scan for nearby houses belonging to the same battery
             for house in houses:
                 waypointHouse = False
                 cable = self.cables[house]
@@ -83,27 +84,33 @@ class Share(Density):
                     if previousY != y:
                         break
                     
-                    # on each point of the cable scan for paralles cabels belonging to the same battery
+                    # on each point of the cable scan for given steps in y direction
                     searchLocations = []
                     for j in range(1,self.verticalSteps + 1):
                         searchLocations.append(f"{x},{y+j}")
                         searchLocations.append(f"{x},{y-j}")
 
+                    # check if location is already scanned
                     for location in searchLocations:
                         if location not in self.houseLocations:
                             continue
-
+                        
+                        # if searchlocation has a house which is already relayed avoid it
                         if self.houseLocations[location] in blacklistHouses:
                             continue
 
+                        # if searchlocation has a legal house save this houseobject as waypoint
                         if self.houseLocations[location] in houses:
                             waypointHouse = self.houseLocations[location]
                             break
                     
+                    # when a legal house is found relay the cable and add the house to the houseblacklist
                     if waypointHouse != False:
                         self.segmentCopy(house, waypointHouse, i)
                         blacklistHouses.append(house)
                         break
+
+        return True 
 
 
     def getHouseLocation(self):
@@ -121,54 +128,48 @@ class Share(Density):
 
     def segmentCopy(self, originHouse, waypointHouse, sidetrackPoint):
         '''
+        Create new cable segment running from the cable relay point to the new house
         '''
+
         newX = []
         newY = []
 
+        # get starting point of new cable segment 
         for i in range(sidetrackPoint + 1):
             newX.append(self.cables[originHouse].x[i])
             newY.append(self.cables[originHouse].y[i])
 
+        # calculate distance to new house in y direction 
         difference = self.cables[originHouse].y[sidetrackPoint] - self.cables[waypointHouse].y[0]
 
+        # save cable segment going in positive y direction 
         if difference > 1:
             for k in range(1,difference):
                 newX.append(newX[-1])
                 newY.append(newY[-1] - 1)
+
+        # save cable segment going in positive y direction 
         elif difference < -1:
             for k in range(1,abs(difference)):
                 newX.append(newX[-1])
                 newY.append(newY[-1] + 1)
 
+        # save final point of new cable segment 
         for x, y in zip(self.cables[waypointHouse].x, self.cables[waypointHouse].y):
             newX.append(x)
             newY.append(y)
         
-
+        # save new cable segment as cable object 
         self.cables[originHouse].x = newX
         self.cables[originHouse].y = newY
-
         self.cables[originHouse].makeLocation()
-
-        """
-        if abs(difference) > 2:
-            print()
-            print(f"difference: {difference}")
-            print(f"sidetrackPoint: {sidetrackPoint}")
-            coordinates = []
-
-            for x, y in zip(newX, newY):
-                coordinates.append(f"{x},{y}")
-
-            print(len(newX) == len(newY))
-            print(coordinates)
-        """
 
         return True
         
     
     def sortHouseDistance(self, houses):
         '''
+        Sorts houses from furthest to closest from battery
         '''
         unsorted = houses
         sorted = []
