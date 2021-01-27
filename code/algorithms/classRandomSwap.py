fff701e5ab803783649b1e14083461b427ea6cbc
koensmallegange <70605029+koensmallegange@users.noreply.github.com>
2021-01-27 16:44:22 +0100

    comments randomswap

--- code/algorithms/classRandomSwap.py
+++ code/algorithms/classRandomSwap.py
@@ -1,14 +1,10 @@
-'''
-Algorithm to improve optimize solution. Keeps swapping two randomly selected houses for certain time. Saves cheapest solution. 
-
-'''
-
-
 import os, sys
 import random
 import time
 import matplotlib.pyplot as plt
 import copy
+
+from classInitialSolution import InitialSolution
 
 directory = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(directory, "code"))
@@ -16,34 +12,27 @@ sys.path.append(os.path.join(directory, "code", "classes"))
 sys.path.append(os.path.join(directory, "code", "algorithms"))
 sys.path.append(os.path.join(directory, "code", "visualisation"))
 
-#from classBattery import Battery
-#from classHouse import House
-#from classGrid import Grid
-#from readBattery import readBattery
-#from readHouse import readHouse
-#from vis import plot
-#from classCable import Cable
-#from bubblesort import bubblesort
-#from bubblesortBattery import bubblesortBattery
-#from classObjective import Objective
-
-from classInitialSolution import InitialSolution
-
 
 class RandomSwap(InitialSolution):
+    '''
+    Optimizes initial solution. Keeps swapping two randomly selected houses for certain time. Saves cheapest solution. 
+
+    '''
 
     def runRandomSwap(self, runtime):
         '''
-        Control flow of random swap algorithm
+        Controls flow of random swap algorithm
         '''
-        # set initial parameters
+        # get a initial solution (from classgrid)
         self.runInitialSolution()
-        #print(f"initial cost: {self.totalCost()}")
+        
+        # set limiting parameters
         self.bestPrice = self.totalCost()
         self.limitPrice = self.bestPrice * 1.001
+
         self.initialCables = self.clone()
         self.cablesBest = self.initialCables
-        
+
         cableSegmentList = self.cableSegmentList()
         cableSegmentSet = set(cableSegmentList)
         self.initialRepeatCableCount = len(cableSegmentList) - len(cableSegmentSet)
@@ -53,30 +42,27 @@ class RandomSwap(InitialSolution):
         prevMinute = 0.0
         reps = 0
 
-        # run swap algorithm for selected runtime
+        # make swaps during selected runtime
         while time.time() < endTime:
             
             # print time
             currentMinute = round((endTime - time.time())/60,1)
             if currentMinute != prevMinute:
                 prevMinute = currentMinute
-                #print(f"remaining minutes: {prevMinute}, repeats: {reps}")
 
-            # make swap 
+            # make one swap 
             self.randomSwap()
             reps += 1
 
-        #print(f"DONE! repeats: {reps}")
-        #print(f"last price: {self.totalCost()}")
-
+        # reset cable configuration to best configuration
         self.replaceData(self.cablesBest)
-        #print(f"best price: {self.totalCost()}")
+
         return True
 
 
     def randomSwap(self):
         '''
-        Makes swap between two houses 
+        Makes one swap between two houses 
         '''
 
         # select two random houses
@@ -84,36 +70,22 @@ class RandomSwap(InitialSolution):
         house0 = self.houses[0]
         house1 = self.houses[1]
         
-        # If swap is possible within capacity of batteries make swap
+        # if swap is possible within capacity of batteries make the swap
         if self.swap(house0, house1):
         
-            # save new configuration
+            # get paramaters of new configuration
             currentPrice = self.totalCost()
             cableSegmentList = self.cableSegmentList()
             cableSegmentSet = set(cableSegmentList)
             currentRepeatCableCount = len(cableSegmentList) - len(cableSegmentSet)
 
-            # check for improvement in configuration
+            # save the new configuration if it is an improvement 
             if currentPrice < self.bestPrice:
                 self.bestPrice = currentPrice
                 self.cablesBest = self.clone()
-                #print(f"better price: {self.totalCost()}")
-                #self.output("bestOut")
-
-            #print(currentRepeatCableCount)
-
-            # if swap exceeds limiting parameters reset to initial configuration
+            
+            # if swap causes total cost to exceed limiting parameters: reset to initial configuration
             if currentPrice > self.limitPrice or currentRepeatCableCount < self.initialRepeatCableCount:
                 self.replaceData(self.initialCables)
-                #self.batteries.clear()
-                #self.cables.clear()
-                #self.addBatteries()
-                #self.runInitialSolution()
-
+            
         return True
-     
-'''
-district 1: min objective: 53188
-district 2: min objective: 45268
-district 3: min objective: 42757
-'''
